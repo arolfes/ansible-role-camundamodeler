fe73c01dd03a8b7465dad84786cfe1d7e1d06268
arolfes <arolfes@users.noreply.github.com>
2021-03-22 16:46:49 +0100

    latest version 4.6.0

--- molecule/cm-max/tests/test_role.py
+++ molecule/cm-max/tests/test_role.py
@@ -8,7 +8,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 @pytest.mark.parametrize('version_dir_pattern', [
-    '4.5.0$'
+    '4.6.0$'
 ])
 def test_cm_installed(host, version_dir_pattern):
 
